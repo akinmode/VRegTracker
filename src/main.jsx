849b652c0733e5081fdc556c8e0a06c1287d8fb0
Akinwale Akinjiola <akinjil@gmail.com>
2025-06-02 16:59:04 +0100

    Update main.jsx
    edited main

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
-
-/// File: src/App.jsx
-import React from 'react';
-import VehiclePaymentSystem from './components/VehiclePaymentSystem';
-
-function App() {
-  return <VehiclePaymentSystem />;
-}
-
-export default App;
