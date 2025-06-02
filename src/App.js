acd19f04088429dd091ef3f55587301b69b59cda
Akinwale Akinjiola <akinjil@gmail.com>
2025-06-02 16:24:23 +0100

    Update App.js
    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import VehiclePaymentSystem from './components/VehiclePaymentSystem';
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <VehiclePaymentSystem />
+    </div>
+  );
+}
+
+export default App;
